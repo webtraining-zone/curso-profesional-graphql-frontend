e0f30dbe0c44693af40a38912459ac2b6948f750
Alex Arriaga <alexzander.arriaga@base22.com>
2018-11-28 21:48:07 -0600

    Agregar ejemplo de integración de Bootstrap 4

--- components/common/Header.js
+++ components/common/Header.js
@@ -3,8 +3,20 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-        <header>
-          Header
+        <header className={"b-header"}>
+          <nav id="navbar" className="navbar navbar-light bg-primary">
+            <div className="container">
+              <a className="navbar-brand" href="#">Library</a>
+              <ul className="nav nav-pills">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Books</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Create</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </header>
     );
   }
